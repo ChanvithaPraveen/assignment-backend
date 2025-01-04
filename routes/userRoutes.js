67aaf2412540628db4835e101a45057fa1ecd0d9
Chanvitha Praveen <chanvithapraween@gmail.com>
2025-01-04 10:12:26 +0530

    initiated the MVC architechure and sketched up the code base

---
+++ routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const {
+  createUser,
+  loginUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+const router = express.Router();
+
+router.post('/register', createUser);
+router.post('/login', loginUser);
+router.get('/', authMiddleware, getAllUsers);
+router.get('/:id', authMiddleware, getUserById);
+router.put('/:id', authMiddleware, updateUser);
+router.delete('/:id', authMiddleware, deleteUser);
+
+module.exports = router;
